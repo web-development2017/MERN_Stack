f774a57cffcaf874387087e30f9c2441af5cdf71
Rich <web_dev2017@outlook.com>
2022-06-13 15:56:07 +0100

    Build sucessful but heroku website not showing app

--- server/index.js
+++ server/index.js
@@ -23,10 +23,10 @@ app.use('/graphql', graphqlHTTP({
 
 if(process.env.NODE_ENV === "production"){
     // app.use(express.static("client/build"));
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, '/client/build')));
 
     app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
