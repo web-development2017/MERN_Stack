d6c0312a3a411ea05c7ca8c08031fcbe6aedf5bd
Rich <web_dev2017@outlook.com>
2022-06-14 20:24:13 +0100

    added uri in client

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache} from "@apollo/client";
 // });
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'https://mernstack888.herokuapp.com/graphql',
   cache: new InMemoryCache()
   // cache
 })
