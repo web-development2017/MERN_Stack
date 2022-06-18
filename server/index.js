46e952e6b71e2a54ff228e5d34ab651a1a42738b
Rich <web_dev2017@outlook.com>
2022-06-18 15:17:49 +0100

    update

--- server/index.js
+++ server/index.js
@@ -30,10 +30,11 @@ if(process.env.NODE_ENV === "production"){
    
     app.use(express.static(path.join(__dirname, '../client/build')));
 
-    app.get('/', function (req, res) {
-        // res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-        res.sendFile(path.join(__dirname+ '../client/build/index.html'));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html'));
+        // res.sendFile(path.join(__dirname+ '../client/build/index.html'));
     });
+    
 }else{
     console.log("Development")
     // app.get('/', (req, res)=>{
