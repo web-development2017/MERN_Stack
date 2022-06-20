f014c840764a93c99934fdbbbb4a2d361e9da451
Rich <web_dev2017@outlook.com>
2022-06-20 16:54:54 +0100

    delete project when client is deleted

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -101,11 +101,11 @@ const mutation = new GraphQLObjectType({
                 id: { type: GraphQLNonNull(GraphQLID) },
             },
             resolve(parent, args){
-                // Project.find({ clientId: args.id }).then((projects) => {
-                //     projects.forEach((project) =>{
-                //         project.remove();
-                //     });
-                // });
+                Project.find({ clientId: args.id }).then((projects) => {
+                    projects.forEach((project) =>{
+                        project.remove();
+                    });
+                });
                 return Client.findByIdAndRemove(args.id);
             }
         },
